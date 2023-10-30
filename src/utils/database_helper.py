30e3be5b81f8e44d006f77c89a9ee7b3b463ad68
Matheus Henrique <matheus1604.mh@gmail.com>
2023-10-30 16:10:28 -0300

    :sparkles: feat: adding function to create table dynamically

--- src/utils/database_helper.py
+++ src/utils/database_helper.py
@@ -1,5 +1,6 @@
 import psycopg2
 from core.settings import settings
+from utils.json_helper import extract_keys, extract_values, extract_table_name
 
 
 def create_connection():
@@ -17,27 +18,20 @@ def create_connection():
         print(error)
 
 
-def create_table(cursor):
-    cursor.execute('DROP TABLE IF EXISTS data;')
+def create_table(con, json_data):
+    cursor = con.cursor()
+
+    table_name = extract_table_name(json_data)
+    cursor.execute(f'DROP TABLE IF EXISTS {table_name};')
+
+    columns = extract_keys(json_data)
+    columns = ', '.join(
+        [f'{column} INTEGER NOT NULL' for column in columns])
+    # columns = str(columns)
     try:
-        cursor.execute('''
-                    CREATE TABLE data (
-                        id INTEGER NOT NULL,
-                        A INTEGER NOT NULL,
-                        B INTEGER NOT NULL
-                    );
-                    ''')
+        cursor.execute(f'''CREATE TABLE {table_name} ({columns});''')
+        con.commit()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
+    return columns
 
-
-def load_data(cursor, json_data):
-    ids = json_data['table']['id']
-    column_a = json_data['table']['A']
-    column_b = json_data['table']['B']
-
-    for i in range(len(ids)):
-        cursor.execute('''
-                       INSERT INTO data (id, A, B)
-                       VALUES (%s, %s, %s);
-                       ''', (ids[i], column_a[i], column_b[i]))
